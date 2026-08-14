2f6994fd679b76e3c8db19c69cd9db7c6baca2ae
mlscmoulika <mlscmoulika@gmail.com>
2026-08-14 23:38:28 +0530

    added benchmarking

--- Leetgpu_submissions/Matrix Addition.py
+++ Leetgpu_submissions/Matrix Addition.py
@@ -1,6 +1,7 @@
 import torch
 import triton
 import triton.language as tl
+import time
 
 
 @triton.jit
@@ -21,7 +22,57 @@ def matrix_add_kernel(A, B, C, N, BLOCK_SIZE: tl.constexpr):
 
 # a, b, c are tensors on the GPU
 def solve(a: torch.Tensor, b: torch.Tensor, c: torch.Tensor, N: int):
-    BLOCK_SIZE = 64
+    BLOCK_SIZE = 512
     
     grid = (triton.cdiv(N, BLOCK_SIZE),triton.cdiv(N, BLOCK_SIZE))
     matrix_add_kernel[grid](a, b, c, N, BLOCK_SIZE)
+
+N = 512
+print("Matrix addition of two matrices of size ", N, "x", N)
+A = torch.randn(N, N, device="cuda")
+B = torch.randn(N, N, device="cuda")
+C = torch.empty(N, N, device="cuda")
+torch.cuda.synchronize()
+t = time.time()
+solve(A, B, C, N)   
+torch.cuda.synchronize()
+print("Time taken for matrix addition on GPU using Triton: ", time.time() - t)
+# solving using torch
+t = time.time()
+C_torch = A + B 
+torch.cuda.synchronize()
+print("Time taken for matrix addition on GPU using PyTorch: ", time.time() - t)
+
+# N = 8192
+# print("Matrix addition of two matrices of size ", N, "x", N)
+# A = torch.randn(N, N, device="cuda")
+# B = torch.randn(N, N, device="cuda")
+# C = torch.empty(N, N, device="cuda")
+# torch.cuda.synchronize()
+# t = time.time()
+# solve(A, B, C, N)   
+# torch.cuda.synchronize()
+# print("Time taken for matrix addition on GPU using Triton: ", time.time() - t)
+# # solving using torch
+# torch.cuda.synchronize()
+# t = time.time()
+# C_torch = A + B 
+# torch.cuda.synchronize()
+# print("Time taken for matrix addition on GPU using PyTorch: ", time.time() - t)
+
+# N = 1024
+# print("Matrix addition of two matrices of size ", N, "x", N)
+# A = torch.randn(N, N, device="cuda")
+# B = torch.randn(N, N, device="cuda")
+# C = torch.empty(N, N, device="cuda")
+# torch.cuda.synchronize()
+# t = time.time()
+# solve(A, B, C, N)   
+# torch.cuda.synchronize()
+# print("Time taken for matrix addition on GPU using Triton: ", time.time() - t)
+# # solving using torch
+# torch.cuda.synchronize()
+# t = time.time()
+# C_torch = A + B 
+# torch.cuda.synchronize()
+# print("Time taken for matrix addition on GPU using PyTorch: ", time.time() - t)
--- Leetgpu_submissions/softmax.py
+++ Leetgpu_submissions/softmax.py
@@ -3,6 +3,7 @@
 import torch
 import triton
 import triton.language as tl
+import time
 
 
 @triton.jit
@@ -24,3 +25,54 @@ def solve(input: torch.Tensor, output: torch.Tensor, N: int):
     ma = torch.max(input).item()
     deno = torch.sum(torch.exp(input - ma)).item()
     softmax_kernel[grid](input,output, N, BLOCK_SIZE, ma, deno)
+
+N = 8192
+print("Matrix addition of two matrices of size ", N, "x", N)
+A = torch.randn(N, device="cuda")
+# B = torch.randn(N, N, device="cuda")
+C = torch.empty(N, device="cuda")
+torch.cuda.synchronize()
+t = time.time()
+solve(A, C, N)   
+torch.cuda.synchronize()
+print("Time taken for matrix addition on GPU using Triton: ", time.time() - t)
+# solving using torch
+torch.cuda.synchronize()
+t = time.time()
+C_torch = torch.softmax(A, dim=0) 
+torch.cuda.synchronize()
+print("Time taken for matrix addition on GPU using PyTorch: ", time.time() - t)
+
+# N = 1024
+# print("Matrix addition of two matrices of size ", N, "x", N)
+# A = torch.randn(N, 1, device="cuda")
+# #B = torch.randn(N, N, device="cuda")
+# C = torch.empty(N, 1, device="cuda")
+# torch.cuda.synchronize()
+# t = time.time()
+# solve(A, C, N)   
+# torch.cuda.synchronize()
+# print("Time taken for matrix addition on GPU using Triton: ", time.time() - t)
+# # solving using torch
+# torch.cuda.synchronize()
+# t = time.time()
+# C_torch = torch.softmax(A, dim=1)
+# torch.cuda.synchronize()
+# print("Time taken for matrix addition on GPU using PyTorch: ", time.time() - t)
+
+# N = 512
+# print("Matrix addition of two matrices of size ", N, "x", N)
+# A = torch.randn(N, 1, device="cuda")
+# # B = torch.randn(N, N, device="cuda")
+# C = torch.empty(N, 1, device="cuda")
+# torch.cuda.synchronize()
+# t = time.time()
+# solve(A, C, N)   
+# torch.cuda.synchronize()
+# print("Time taken for matrix addition on GPU using Triton: ", time.time() - t)
+# # solving using torch
+# torch.cuda.synchronize()
+# t = time.time()
+# C_torch = torch.softmax(A, dim=1) 
+# torch.cuda.synchronize()
+# print("Time taken for matrix addition on GPU using PyTorch: ", time.time() - t)
